3dce6c01e414f2a2d4b2988eb2501b362c66bb28
Newton Ombese <newtonombese1@gmail.com>
2022-03-10 07:03:40 -0800

    corrections

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starting point of program
+ *
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
