e50ebdfe3595dc72021742cace2c5205f516ba9b
Newton Ombese <newtonombese1@gmail.com>
2022-03-10 06:36:54 -0800

    prog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry Point
+
+ *
+
+ * Return: returns 0 if code executes successfully
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
