bc2a452dee9f348843b86901c46bdfb5d06f7cbc
Newton Ombese <newtonombese1@gmail.com>
2022-03-21 04:41:45 -0700

    b

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates a pointer's pointed value to 98
+ * @n: pointer pointing to value that needs to be changed
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
