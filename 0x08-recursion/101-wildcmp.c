594a2d03e47d6304ae03e045e9fac88c02a2d77c
NewtonOmbese <newtonombese1@gmail.com>
2022-03-30 00:19:41 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -26,7 +26,6 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
 		return (1);
 
-
 	/**
 	 * if the chars are equal in both strings,
 	 * if we reached the end of s1, return 1
@@ -44,6 +43,7 @@ int wildcmp(char *s1, char *s2)
 	 */
 	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
+
 	/**
 	 * if the char in s2 is a *
 	 * finds the address of the first char after the *
