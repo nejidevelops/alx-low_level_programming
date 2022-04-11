2c80188dd83b205da0fcaa0dc5ba29299393d4d4
NewtonOmbese <newtonombese1@gmail.com>
2022-04-11 08:34:02 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,8 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef abs
+#define abs
 
-#define ABS(x) ((x) > (-1) ? (x) : (x * -1))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
-#endif
+#endif /* abs */
+
+
