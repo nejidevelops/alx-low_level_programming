c7fa7898501dc87f2bcbe3bdf7a1a11b74671463
NewtonOmbese <newtonombese1@gmail.com>
2022-04-11 08:30:02 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the program it was compiled from
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
